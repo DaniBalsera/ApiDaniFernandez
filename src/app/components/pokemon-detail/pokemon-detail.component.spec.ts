7fab61e00477d133ac82c22150309b08e833d354
DaniBalsera <99357708+DaniBalsera@users.noreply.github.com>
2022-11-29 14:25:20 +0100

    Update pokemon-detail.component.spec.ts

--- src/app/components/pokemon-detail/pokemon-detail.component.spec.ts
+++ src/app/components/pokemon-detail/pokemon-detail.component.spec.ts
@@ -3,6 +3,8 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { PokemonDetailComponent } from './pokemon-detail.component';
 
 describe('PokemonDetailComponent', () => {
+  
+  /*Let--> tipo de dato que permite declarar variables limitando su alcance al bloque, declaración, o expresión donde se está usando*/
   let component: PokemonDetailComponent;
   let fixture: ComponentFixture<PokemonDetailComponent>;
 
